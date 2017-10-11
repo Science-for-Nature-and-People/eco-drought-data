c501b1254a88432432d3e5a6c44d8919e7909ee2
Julien Brun <brun@nceas.ucsb.edu>
2017-10-11 11:00:24 -0700

    clip reproject predictors

---
+++ reading_predicotrs.R
@@ -0,0 +1,88 @@
+library(tidyverse)
+library(sf)
+library(sp)
+# devtools::install_github("tidyverse/ggplot2")
+library(ggplot2)
+library(raster)
+library(ncdf4)
+library(RColorBrewer)
+
+predcit_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Predictors"
+domain_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Domain/"
+
+# Predicors
+swe_file <- "shelley.swe"
+soil_file <- "shelley.soil"
+
+
+## Domain ----
+
+huc8_file_1 <- "UMH_HUC8/wbdhu8_a_10020001.shp"
+uhm_file <- "UMH_Basins_outside_boundary_shapefile/UMH_Basins_outside_boundary.shp"
+
+# Read Domain data
+umh <- st_read(file.path(domain_dir,umh_boundary_file))
+huc8_1 <- st_read(file.path(domain_dir,huc8_file_1))
+
+# plot
+ggplot() +
+  geom_sf(data=umh, aes(fill = HUC8)) +
+  geom_sf(data=huc8_1, aes(fill = HUC8)) 
+
+
+## Predicors ----
+swe_file <- "shelley.swe"
+soil_file <- "shelley.soil"
+
+# Read Predictor data
+swe_data <- read_table(file.path(predcit_dir,swe_file),col_names = TRUE)
+soil_data <- read_table(file.path(predcit_dir,soil_file),col_names = TRUE)
+
+
+# Read the netcf tmax
+ncin <- nc_open(file.path(predcit_dir,"tmax_anomalies_2000_MissouriHeadwaters_JJA.nc"))
+# get the lat/long
+lon <- ncvar_get(ncin, varid = "lon")
+lat <- ncvar_get(ncin, varid = "lat")
+# get the variable
+tmp_array <- ncvar_get(ncin,"tmax")
+
+image(lon,lat,tmp_array, col=rev(brewer.pal(10,"RdBu")))
+
+# Read the netcf tmin
+ncin <- nc_open(file.path(predcit_dir,"tmin_anomalies_2000_MissouriHeadwaters_DJF.nc"))
+# get the lat/long
+lon <- ncvar_get(ncin, varid = "lon")
+lat <- ncvar_get(ncin, varid = "lat")
+# get the variable
+tmp_array <- ncvar_get(ncin,"tmin")
+
+image(lon,lat,tmp_array, col=rev(brewer.pal(10,"RdBu")))
+
+
+# Read the netcf precip
+ncin <- raster(file.path(predcit_dir,"Precip_AnnualAnomalies2000.nc"))
+# # get the lat/long
+# lon <- ncvar_get(ncin, varid = "lon")
+# lat <- ncvar_get(ncin, varid = "lat")
+# # get the variable
+# tmp_array <- ncvar_get(ncin,"precip")
+# image(lon,lat,tmp_array, col=rev(brewer.pal(10,"RdBu")))
+image(ncin, col=rev(brewer.pal(10,"RdBu")))
+
+
+# Read the netcf EDDI (evoparitve demand drought index)
+ncin <- raster(file.path(predcit_dir,"EDDI_MJJAS_2000.nc"))
+# get the lat/long
+# lon <- ncvar_get(ncin, varid = "lon")
+# lat <- ncvar_get(ncin, varid = "lat")
+# # get the variable
+# tmp_array <- ncvar_get(ncin,"EDDI")
+#image(lon,lat,tmp_array, col=rev(brewer.pal(10,"RdBu")))
+image(ncin, col=rev(brewer.pal(10,"RdBu")))
+
+# Raster climatic water deficit
+
+def_sum <- raster(file.path(predcit_dir, "def_sum_1980.tif"))
+image(def_sum)
+def_sum
---
+++ reproject_clip.R
@@ -0,0 +1,88 @@
+library(tools)
+library(foreach)
+# library(rgdal)
+library(raster)
+# library(ggplot2)
+# library(rgeos)
+# library(mapview)
+# library(leaflet)
+# library(broom) # if you plot with ggplot and need to turn sp data into dataframes
+library(sf)
+
+library(doParallel)
+registerDoParallel(12)
+
+options(stringsAsFactors = FALSE)
+
+
+### CONSTANTS ----
+
+## Path
+predcit_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Predictors"
+domain_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Domain"
+response_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Response"
+
+predictor_files <- c("nlcd_2001_impervious_2011_edition_2014_10_10/nlcd_2001_impervious_2011_edition_2014_10_10.img",
+                     "nlcd2001_canopy_mosaic_1-29-08/nlcd_canopy_mosaic_1-29-08.img",
+                     #"lf3_30T_GNLCC/lf3_30T_GNLCC.tif",
+                     "UMH/evh/w001001.adf",
+                     "UMH/evt/w001001.adf")
+
+out_dir <- "ProjectClipMask"
+out_dir_full <- file.path(predcit_dir, out_dir)
+# Create the directory
+dir.create(out_dir_full, showWarnings = FALSE)
+
+## Shapefile
+# Domain
+Domain_shp_file <- file.path(domain_dir,"UMH_Basins_outside_boundary_shapefile/UMH_Basins_outside_boundary.shp")
+
+## Raster
+# Response
+NDVI_trends_file <- "UMHbasins_aYs_2005_2006_jDs_200_250_epoch5_NDVI_landsat_TDDlm1.tif"
+# lf3_file <- "lf3_30T_GNLCC/lf3_30T_GNLCC.tif"
+
+## CRS
+EPSG_conus_alb_83 <- 5070
+rough_extent <- extent(-115, 42, -108, 48)
+  
+### MAIN ----
+#read domain
+domain_shp <- st_read(Domain_shp_file)
+st_bbox(domain_shp)
+
+# reproject to conus albers NAD 83
+domain_shp_83 <- st_transform(domain_shp, EPSG_conus_alb_83)
+st_bbox(domain_shp_83)
+        
+# Output layer
+st_write(domain_shp_83, 
+         dsn = file.path(domain_dir,"UMH_Basins_outside_boundary_shapefile/UMH_Basins_NAD83_outside_boundary.shp"),
+         driver = "ESRI Shapefile", delete_layer = TRUE
+         )
+
+# Load the trnds to be used as mask
+ndvi_trends <- raster(file.path(response_dir,NDVI_trends_file))
+
+
+foreach(i=1:length(predictor_files)) %dopar% {
+  data_file <- predictor_files[i]
+  # Load the file to be masked
+  data_raster <- raster(file.path(predcit_dir, data_file))
+  # lf3_crop_rough <- crop(x = lf3, y = rough_extent) 
+  # Reproject and clip
+  data_raster_5070 <- projectRaster(data_raster, ndvi_trends, method = 'ngb')
+  # mask tree only
+  masked <- mask(data_raster_5070, ndvi_trends)
+  #Build ouptu name
+  if (file_ext(data_file) == "adf"){
+    parts <- str_split(data_file,"/")[[1]]
+    file_name <- paste(parts[1],parts[2],sep="_")
+    outputfile_name <- paste0(file_name,"_epsg5070_masked.tif")
+  }else{
+    outputfile_name <- basename(paste0(file_path_sans_ext(data_file), "_epsg5070_masked.tif"))
+  }
+  outputfile_name_full <- file.path(out_dir_full,outputfile_name)
+  # outputfile_name_full
+  writeRaster(masked, filename=outputfile_name_full, format="GTiff", overwrite=TRUE, options="COMPRESS=LZW")
+}
