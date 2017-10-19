2673564a9cec892a30e2162c7e0b549902554f02
Julien Brun <brun@nceas.ucsb.edu>
2017-10-19 15:49:09 -0700

    combine huc8

---
+++ combine_huc8.R
@@ -0,0 +1,56 @@
+library(raster)
+library(sf)
+
+## CONSTANTS ----
+
+# Main directories
+predict_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Predictors"
+domain_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Domain"
+response_dir <- "/home/shares/ecodrought/VulnerabilityAnalysis/Response"
+
+# Full path to HUC8 shapefiles
+HUC8_dir_full <- file.path(domain_dir,"UMH_HUC8")
+
+# raster grid to match
+NDVI_trends_file <- "UMHbasins_aYs_2005_2006_jDs_200_250_epoch5_NDVI_landsat_TDDlm2.tif"
+
+# Projection
+EPSG_conus_alb_83 <- 5070
+
+## FUNCTIONS ----
+
+#' Combine a list of non overlaping shapefiles into one
+#'
+#' @param top_dir a character string to the directory containing the shapefiles to merge
+#'
+#' @return simple feature object
+#' @export
+#'
+#' @examples combine_shapefiles("/home/shares/ecodrought/VulnerabilityAnalysis/Domain/UMH_HUC8")
+combine_shapefiles <- function(top_dir){
+  # List all the cliped datasets
+  shp_files <- list.files(path = top_dir, pattern = "*.shp", full.names = TRUE)
+  # Read the data in a list
+  shp_List <- lapply(shp_files, st_read)
+  # Combine the data
+  shp_all <-  do.call("rbind",shp_List)
+  return(shp_all)
+}
+
+
+## MAIN ----
+
+# Combine shapefiles
+umh_huc8_all <- readOGR(HUC8_dir_full, "umh_huc8_all")
+
+# Reproject
+umh_huc8_all_epsg_5070 <- spTransform(umh_huc8_all, crs(ndvi_trends))
+
+
+# rasterize
+ndvi_trends <- raster(file.path(response_dir, NDVI_trends_file))
+huc8_rast <- rasterize(umh_huc8_all_epsg_5070, ndvi_trends, filename=file.path(HUC8_dir_full, "umh_huc8.tif"))
+
+
+
+
--- to_csv.R
+++ to_csv.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(stringr)
 library(raster)
+library(RColorBrewer)
+
 rasterOptions(maxmemory = 1e10)
 
 # Number of pixels
@@ -21,8 +23,25 @@ input_rasters <- list.files(path = input_dir_full, pattern = "*.tif", full.names
 
 ## Create a list of ids with values eg tree area
 # Response
-NDVI_trends_file <- "UMHbasins_aYs_2005_2006_jDs_200_250_epoch5_NDVI_landsat_TDDlm1.tif"
+NDVI_trends_file <- "UMHbasins_aYs_2005_2006_jDs_200_250_epoch5_NDVI_landsat_TDDlm2.tif"
+NDVI_R2_file <- "UMHbasins_aYs_2005_2006_jDs_200_250_epoch5_NDVI_landsat_TDDlr2R2.tif"
+
 ndvi_trends <- raster(file.path(response_dir,NDVI_trends_file))
+NDVI_R2 <- raster(file.path(response_dir,NDVI_R2_file))
+
+## polts 
+#R2
+hist(NDVI_R2[])
+cuts=c(0.6,0.75,0.8)
+pal <- colorRampPalette(c("blue","red"))
+plot(NDVI_R2)
+plot(NDVI_R2, breaks=cuts, col = pal(3), add=TRUE)
+#trends
+hist(ndvi_trends[])
+plot(ndvi_trends)
+cuts_trends <- c(-1,-0.5,-0.25,0,0.25,0.5,1)
+plot(ndvi_trends, breaks=cuts_trends, col = pal(7))
+
 
 # Initialize the data.table
 DTmask <- data.table(pixel_id = 1:nb_pix)
@@ -32,7 +51,6 @@ setkey(DTmask,pixel_id)
 DTmask[, mask:=!is.na(getValues(ndvi_trends))]
 # List the values
 keys_id <- DTmask[mask==TRUE, pixel_id]
-
 
 # Initialize the dataframe
 DT <- data.table(pixel_id = keys_id)
