e2622025f12759cf205df04db3ab8212ae680862
Nathan Hwangbo <nathanhwangbo@ucsb.edu>
2019-08-14 16:53:42 -0700

    Clarify workflow to deal with projection inconsistencies

--- great_plains_processing.R
+++ great_plains_processing.R
@@ -30,34 +30,16 @@ filenames <- RCurl::getURL(url, ftp.use.epsv = FALSE, dirlistonly = TRUE) %>%
   stringr::str_split('\n') %>% unlist
 
 #[1] is just an example. can easily generalize this to a loop if we want all files, but very expensive
-#leri_filepath <- paste0('/Users/nathan/Downloads/', filenames[1])
 #downloads all the files into a temp location
-leri_filepath <- tempfile(fileext = '.zip')
+leri_filepath <- paste(tempdir(), filenames[1], sep = '/')
+leri_filepath <- file.path(tempdir(), filenames[1])
 utils::download.file(paste0(url, filenames[1]), destfile = leri_filepath)
 
 leri_data <- raster::stack(leri_filepath)
-
+#file.remove(leri_filepath)
 
 #https://climatedataguide.ucar.edu/climate-data/standardized-precipitation-index-spi
 #???
-
-
-#############################################
-
-### get rasters all in same format? ###
-
-#############################################
-
-#use the finest resolution
-resolution <- pmin(raster::res(eddi_data), raster::res(leri_data))
-
-#convert raster to same as above
-new_eddi <- raster::projectRaster(eddi_data, crs = raster::crs(leri_data), res = resolution)
-
-
-
-
-
 
 
 
@@ -68,9 +50,6 @@ new_eddi <- raster::projectRaster(eddi_data, crs = raster::crs(leri_data), res =
 #############################################
 #domain_shapefile_path <- '/Users/nathan/Downloads/CNS_GP_RMDSW/CNS_RMDSW_studyarea.shp'
 
-#set a constant coordinate reference system (EPSG:5070) so all our objects are comparable
-EPSG_conus_alb_83 <- 5070
-
 #create temp file path to hold the file.
 path <- tempfile(fileext = '.zip')
 nceas_drive_dribble <- googledrive::team_drive_get(name = 'NCEAS-SciComp')
@@ -80,10 +59,46 @@ cns_files <- unzip(path, exdir = tempdir())
 
 #the shapefile we need has name "CNS_GP_states.shp. we extract it and read it into r
 domain_shapefile_path <- cns_files[stringr::str_which(cns_files, 'CNS_GP_states.shp$')]
+
 domain_shapefile <- sf::st_read(domain_shapefile_path) %>%
-  sf::st_transform(crs = raster::crs(leri_data))
+  sf::st_transform(crs=5070)
 
 #plot(domain_shapefile$geometry)
+
+#############################################
+
+### get rasters all in same format? ###
+
+#############################################
+
+# Step 1. Put the rasters in a list and convert them to a consistent coordinate system (EPSG 5070)
+
+stack_list <- list(eddi_data, leri_data)
+stack_list_epsg <- purrr::map(stack_list, ~raster::projectRaster(.x, crs = "+init=epsg:5070"))
+
+# Step 2. Crop them all to the boundary shape via masking
+
+stack_list_cropped <- purrr::map(stack_list_epsg, ~raster::mask(.x, domain_shapefile) %>%
+                                  raster::crop(raster::extent(domain_shapefile)))
+
+
+# Step 3. Pick the one with finest resolution (this is our template, which we will use to match resolution/extent)
+
+pixel_area_list <- purrr::map(stack_list_cropped, ~ prod(raster::res(.x)))
+template <- stack_list_cropped[[which.min(pixel_area_list)]]
+
+
+# Step 4. Match resolution/match extent even closer between the stacks and the template
+
+stack_list_uniform <- purrr::map(stack_list_cropped, ~raster::resample(.x, template))
+
+
+
+
+
+
+
+
 
 
 
@@ -93,16 +108,31 @@ domain_shapefile <- sf::st_read(domain_shapefile_path) %>%
 ### cropping the raster ###
 
 #############################################
-#' @param data should be a rasterstack object
+#' @param stack should be a rasterstack object
 #' @param domain_shape should be a shapefile that specifies the domain (must have consistent crs)
-mask_and_crop <- function(data, domain_shape){
+#' @param resolution should be the the minimum resolution of all the stacks (a 2 element numeric vector for x,y)
+#' @param espg should be the espg code to use as the crs (4 numbers)
+project_mask_crop_save <- function(stack, domain_shape, espg, resolution){
+  #rasters need espg in a different format than shapefiles
+  espg_crs <- paste0("+init=espg:",espg)
+  projected_stack <-  raster::projectRaster(stack, crs = espg_crs, res = resolution)
+  
   #create a mask (eg create a new raster by overlaying the eddi_data values onto the domain shapefile)
-  mask <- raster::mask(data, domain_shape)
+  mask <- raster::mask(projected_stack, domain_shape)
   #now crop this mask so that it fits into the box that we define earlier
-  raster::crop(mask, raster::extent(domain_shape))
+  cropped <- raster::crop(mask, raster::extent(domain_shape))
+  
+  raster::writeRaster()
 }
 
-eddi_cropped <- mask_and_crop(eddi_data, domain_shapefile)
-leri_cropped <- mask_and_crop(leri_data, domain_shapefile)
+#test
+leri_cropped <- project_mask_crop_save(leri_data, domain_shapefile, 5070, resolution)
+
+raster::plot(leri_cropped)
+
+
+
+
+
 
 
